b03e2a17ac63905846627a059316d2acaa425096
David Sarhid <daviidsarhid@gmail.com>
2020-12-14 02:01:17 -0300

    Agregue menu Recepcion

--- Recepcion.cpp
+++ Recepcion.cpp
@@ -1,1 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void menu(){
+	printf("\nModulo del Asistente ( Recepcion )\n\n");
+	printf("\n=========================\n\n");
+	printf("1.- Iniciar Sesion.\n");
+	printf("2.- Registrar Mascota.\n");
+	printf("3.- Registrar Turno.\n");
+	printf("4.- Listado de Atenciones por Veterinario y Fecha.\n");
+	printf("5.- Cerrar la aplicacion.\n\n");
+	printf("Ingrese opcion: ");
+
+}
+
